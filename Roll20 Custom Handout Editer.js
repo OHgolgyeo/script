217416d5ef55bd50dc745b9efa72ebb242e7089d
OHgolgyeo <5golgyeo@gmail.com>
2026-08-30 08:24:55 +0900

    Update Roll20 Custom Handout Editer.js

--- Roll20 Custom Handout Editer.js
+++ Roll20 Custom Handout Editer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Roll20 Custom Handout Editer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @author       오골계 (https://x.com/5golgyeo)
 // @description  기존의 핸드아웃 편집창에 몇 가지 기능을 추가하고 오류를 수정했습니다. (지원 기능: 본문 이미지 첨부(URL 입력/파일 선택/드래그앤드롭/라이브러리 드래그 지원), 폰트와 크기 지정, 색상 선택 기능 추가, 표 너비·높이·정렬 변경, 표 칸 배경색·테두리 지정, 표 칸 합치기·나누기, 가름줄(구분선) 색상·두께·모양 변경, 템플릿 저장·불러오기(실제 내용 미리보기 지원), 구글 문서 붙여넣을 시 양식 깨지는 오류 수정)
 // @match        https://app.roll20.net/editor/*
@@ -131,7 +131,7 @@ const CUSTOM_FONTS = [
        ========================================================================== */
     const handlePasteFormatting = (e) => {
         const activeEl = document.activeElement;
-        if (!activeEl || (!activeEl.classList.contains('note-editable') && !activeEl.closest('.note-editable'))) return;
+        if (!activeEl || !activeEl.closest('.note-editable, .tox-edit-area')) return;
 
         const clipboardData = e.clipboardData || window.clipboardData;
         if (!clipboardData) return;
